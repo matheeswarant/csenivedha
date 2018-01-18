b88e15a9517f45c062ac78562ea25afb3102835e
Nivedhashree30 <35332022+Nivedhashree30@users.noreply.github.com>
2018-01-18 16:53:17 +0530

    Create vowel.c

---
+++ vowel.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+char c;
+int is Lowercasevowel,is uppercasevowel;
+printf("Enter an alphabet:");
+scanf("%c",&c);
+is Lowercasevowel=(c=='a'||c=='e'||c=='i'||c=='o'||c=='u')
+is uppercasevowel=(c=='A'||c=='E'||c=='I'||c=='O'||c=='U')
+if (is Lowercasevowel||is uppercasevowel)
+printf("%c is consonant";();
+return0;
+}
